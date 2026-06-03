a4dc43d18082aec26a5a8e031571e8fc1e550aed
Israel Albino Galvan <israel.galvan@live.com>
2026-06-02 23:15:22 -0300

    chore(bindings): regenerate ke_input_actions with programmatic add_action/bind_* slots

--- src/csharp/KernelEngine.Framework/Native/Generated/Framework/ke_input_actions.cs
+++ src/csharp/KernelEngine.Framework/Native/Generated/Framework/ke_input_actions.cs
@@ -10,6 +10,21 @@ public unsafe partial struct ke_input_actions
     [NativeTypeName("int32_t (*)(struct ke_input_actions *, const char *)")]
     public delegate* unmanaged[Cdecl]<ke_input_actions*, sbyte*, int> get_action_id;
 
+    [NativeTypeName("int32_t (*)(struct ke_input_actions *, const char *, ke_action_type)")]
+    public delegate* unmanaged[Cdecl]<ke_input_actions*, sbyte*, ke_action_type, int> add_action;
+
+    [NativeTypeName("ke_result (*)(struct ke_input_actions *, int32_t, ke_key)")]
+    public delegate* unmanaged[Cdecl]<ke_input_actions*, int, ke_key, ke_result> bind_key;
+
+    [NativeTypeName("ke_result (*)(struct ke_input_actions *, int32_t, ke_mouse_button)")]
+    public delegate* unmanaged[Cdecl]<ke_input_actions*, int, ke_mouse_button, ke_result> bind_mouse_button;
+
+    [NativeTypeName("ke_result (*)(struct ke_input_actions *, int32_t, ke_key, ke_key)")]
+    public delegate* unmanaged[Cdecl]<ke_input_actions*, int, ke_key, ke_key, ke_result> bind_key_pair;
+
+    [NativeTypeName("ke_result (*)(struct ke_input_actions *, int32_t, ke_key, ke_key, ke_key, ke_key)")]
+    public delegate* unmanaged[Cdecl]<ke_input_actions*, int, ke_key, ke_key, ke_key, ke_key, ke_result> bind_key_quad;
+
     [NativeTypeName("ke_result (*)(struct ke_input_actions *, const ke_input_snapshot *, ke_input_action_event_func, void *)")]
     public delegate* unmanaged[Cdecl]<ke_input_actions*, ke_input_snapshot*, delegate* unmanaged[Cdecl]<void*, ke_input_action_event, void>, void*, ke_result> evaluate;
 
